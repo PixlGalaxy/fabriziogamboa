dee654e32ac75d3afa8be11033958c2d758a9397
PixlGalaxy <fabrizio.gamboa.p@gmail.com>
2025-02-18 12:53:57 -0500

    Fixed Scroll To Top On New Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound"; 
 import About from "./pages/About";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 const App = () => (
   <Router>
+    <ScrollToTop />
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="*" element={<NotFound />} /> {/* 404 */}
-
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </Router>
 );
+
 export default App;
