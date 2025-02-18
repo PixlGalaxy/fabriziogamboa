d1e09352cad60a9422110470b7462c633ad79815
PixlGalaxy <fabrizio.gamboa.p@gmail.com>
2025-02-18 09:05:03 -0500

    Updated Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { ReactComponent as Logo } from "../assets/Icon.svg"; // Asegúrate de que el archivo esté en src/assets
+import { ReactComponent as Logo } from "../assets/Icon.svg";
 
 const Navbar: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
@@ -9,18 +9,15 @@ const Navbar: React.FC = () => {
 
   return (
     <nav className="fixed top-0 left-0 right-0 bg-gray-900 bg-opacity-95 shadow-sm backdrop-blur-sm z-50">
-      {/* Agregamos un ancho máximo para que el contenido no quede centrado en toda la pantalla */}
       <div className="mx-auto px-4 py-4">
-        {/* Usamos justify-between para repartir el espacio entre la izquierda y la derecha */}
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-6">
-            {/* Logo: Siempre visible */}
+
+            {/* Logo */}
             <Link to="/" className="flex items-center">
               <Logo className="logo w-10 h-10 flex-shrink-0" />
-              <span className="ml-2 text-white font-bold text-xl">
-                Fabrizio Gamboa
-              </span>
             </Link>
+
             {/* Desktop Navigation Links */}
             <div className="hidden md:flex items-center space-x-6">
               <Link
@@ -56,7 +53,7 @@ const Navbar: React.FC = () => {
             </div>
           </div>
 
-          {/* Mobile Hamburger (se mantiene a la derecha) */}
+          {/* Mobile Hamburger */}
           <div className="flex md:hidden items-center">
             <button onClick={toggleMenu} className="text-gray-300 hover:text-blue-500">
               {isMenuOpen ? (
@@ -72,7 +69,7 @@ const Navbar: React.FC = () => {
           </div>
         </div>
 
-        {/* Menú de navegación Mobile */}
+        {/* Mobile Menu */}
         {isMenuOpen && (
           <div className="md:hidden mt-4">
             <div className="flex flex-col space-y-3 text-center">
